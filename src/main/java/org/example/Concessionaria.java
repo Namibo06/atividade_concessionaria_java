f86b3de8a235d49c8c0a4c3131bcd259d9700692
LAB 2 <joca@gmail.com>
2025-02-10 19:58:03 -0300

    feat: scanner for input

--- src/main/java/org/example/Concessionaria.java
+++ src/main/java/org/example/Concessionaria.java
@@ -1,8 +1,21 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Concessionaria {
     public static void main(String[] args) {
-        Carro meuCarro = new Carro("Toyota","Corolla",2022);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite a marca: ");
+        String marca = scan.nextLine();
+
+        System.out.println("Digite o modelo: ");
+        String modelo = scan.nextLine();
+
+        System.out.println("Digite o ano: ");
+        int anoModelo = scan.nextInt();
+
+        Carro meuCarro = new Carro(marca,modelo,anoModelo);
         meuCarro.acelerar();
         meuCarro.acelerar();
         meuCarro.acelerar();
